d0012f0bcd4dd0b1d8616fca0b67e5eb3c62a097
nifanic <nifanic@masterpage.net>
2020-05-14 01:50:13 -0400

    Create _Core’s_ `src/index.ts`

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+import { Alpha } from "@nifanic-play/alpha";
+import { Beta } from "@nifanic-play/beta";
+import { Gamma } from "@nifanic-play/gamma";
+
+console.log(new Alpha());
+console.log(new Beta());
+console.log(new Gamma());
